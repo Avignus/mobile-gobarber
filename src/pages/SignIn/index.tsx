ee305ed9eec1bdd419a91f816a0a6eb9feea919a
Avignus <igor.rapozo@gmail.com>
2020-11-27 15:02:51 -0300

    Go barber, tela de login

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { Container, Title } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+    return (
+        <Container>
+            <Image source={logoImg} />
+            <Title>Faça seu logon</Title>
+            <Input name="mail" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+                onPress={() => {
+                    console.log('funcionou!');
+                }}
+            >
+                Entrar
+            </Button>
+        </Container>
+    );
+};
+
+export default SignIn;
